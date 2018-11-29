e8462288577891377df43d198dde65163b4a5fb4
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-11-29 10:25:54 -1000

    pageHeader added and routes working

--- ng-rolodex/src/app/app.module.ts
+++ ng-rolodex/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PageHeaderComponent } from './pages/pageHeader/pageHeader.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ ng-rolodex/src/app/pages/pageHeader/pageHeader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-header',
+    templateUrl: './pageHeader.component.html',
+    styleUrls: ['./pageHeader.component.scss']
+})
+
+export class PageHeaderComponent {
+    title: string = 'Header Component';
+    constructor() {
+    }
+} 
