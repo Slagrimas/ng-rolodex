d85c3ff4dab05b7fd2470ed6bdc0bdbbf34b6403
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-11-29 09:59:24 -1000

    login

--- ng-rolodex/src/app/app.module.ts
+++ ng-rolodex/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
